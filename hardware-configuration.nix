bbc070574fc6d9425a07f414b76c94b0aeb111e2
criftshift <criftshift.net@gmail.com>
2024-02-24 17:33:20 +0100

    Updated Hardware-Conf

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -10,10 +10,21 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-   networking.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp16s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp17s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-}
+
+  fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-label/main";
+        fsType = "ext4";
+      };
+      "/boot" = {
+        device = "/dev/disk/by-label/BOOT";
+        fsType = "vfat";
+      };
+    };
+}g
